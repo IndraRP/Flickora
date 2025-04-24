e53a73f8e066c144544a6df27fd53556def96bbe
IndraRP <indraridho08@gmail.com>
2025-04-24 22:11:55 +0700

    bug route chat detail

--- resources/views/vendor/Chatify/layouts/listItem.blade.php
+++ resources/views/vendor/Chatify/layouts/listItem.blade.php
@@ -24,39 +24,45 @@
     $lastMessageBody = mb_convert_encoding($lastMessage->body, "UTF-8", "UTF-8");
     $lastMessageBody = strlen($lastMessageBody) > 30 ? mb_substr($lastMessageBody, 0, 30, "UTF-8") . ".." : $lastMessageBody;
     ?>
-    <table class="messenger-list-item" data-contact="{{ $user->id }}">
-        <tr data-action="0" data-id="{{ $user->id }}">
-            {{-- Avatar side --}}
-            <td style="position: relative">
-                @if ($user->active_status)
-                    <span class="activeStatus"></span>
-                @endif
-                <div class="avatar av-m" style="background-image: url('{{ $user->avatar }}');">
-                    {{-- <div class="avatar av-m" style="background-image: url('{{ str_starts_with($user->avatar, "http") ? $user->avatar : asset("storage/users-avatar/" . $user->avatar) }}');"> --}}
 
-                </div>
-            </td>
-            {{-- center side --}}
-            <td>
-                <p data-id="{{ $user->id }}" data-type="user">
-                    {{ strlen($user->name) > 12 ? trim(substr($user->name, 0, 12)) . ".." : $user->name }}
-                    <span class="contact-item-time" data-time="{{ $lastMessage->created_at }}">{{ $lastMessage->timeAgo }}</span>
-                </p>
-                <span>
-                    {{-- Last Message user indicator --}}
-                    {!! $lastMessage->from_id == Auth::user()->id ? '<span class="lastMessageIndicator">You :</span>' : "" !!}
-                    {{-- Last message body --}}
-                    @if ($lastMessage->attachment == null)
-                        {!! $lastMessageBody !!}
-                    @else
-                        <span class="fas fa-file"></span> Attachment
+    <a href="/chatify/{{ $user->id }}" class="text-decoration-none">
+        <table class="messenger-list-item" data-contact="{{ $user->id }}">
+            <tr data-id="{{ $user->id }}">
+                {{-- Avatar side --}}
+                <td style="position: relative">
+                    @if ($user->active_status)
+                        <span class="activeStatus"></span>
                     @endif
-                </span>
-                {{-- New messages counter --}}
-                {!! $unseenCounter > 0 ? "<b>" . $unseenCounter . "</b>" : "" !!}
-            </td>
-        </tr>
-    </table>
+                    <div class="avatar av-m" style="background-image: url('{{ $user->avatar }}');">
+                        {{-- <div class="avatar av-m" style="background-image: url('{{ str_starts_with($user->avatar, "http") ? $user->avatar : asset("storage/users-avatar/" . $user->avatar) }}');"> --}}
+
+                    </div>
+                </td>
+
+                {{-- center side --}}
+                <td>
+                    <p data-id="{{ $user->id }}" data-type="user">
+                        {{ strlen($user->name) > 12 ? trim(substr($user->name, 0, 12)) . ".." : $user->name }}
+                        <span class="contact-item-time" data-time="{{ $lastMessage->created_at }}">{{ $lastMessage->timeAgo }}</span>
+                    </p>
+                    <span>
+                        {{-- Last Message user indicator --}}
+                        {!! $lastMessage->from_id == Auth::user()->id ? '<span class="lastMessageIndicator">You :</span>' : "" !!}
+                        {{-- Last message body --}}
+                        @if ($lastMessage->attachment == null)
+                            {!! $lastMessageBody !!}
+                        @else
+                            <span class="fas fa-file"></span> Attachment
+                        @endif
+                    </span>
+                    {{-- New messages counter --}}
+                    {!! $unseenCounter > 0 ? "<b>" . $unseenCounter . "</b>" : "" !!}
+                </td>
+            </tr>
+
+        </table>
+    </a>
+
 @endif
 
 {{-- -------------------- Search Item -------------------- --}}
